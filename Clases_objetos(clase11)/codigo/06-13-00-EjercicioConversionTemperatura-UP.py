afe77bbb99f67d1f5b3217ea1971b8aa7434b4ef
“guillermopetcho” <guillermopetcho@gmail.com>
2024-09-04 23:48:57 -0300

    c

--- Clases_objetos(clase11)/codigo/06-13-00-EjercicioConversionTemperatura-UP.py
+++
@@ -1,4 +0,0 @@
-"""
-Ejercicio: Convertidor de Temperatura
-Realizar dos funciones para convertir de grados celsius a fahrenheit y viceversa.
-"""
--- Clases_objetos(clase11)/codigo/09-03-00-EjemploParametros-UP.py
+++
@@ -1,8 +0,0 @@
-print('*** Funciones en Python ***')
-
-# Definir una funcion para mandar a saludar
-def saludar(mensaje):
-   print(f'Mensaje recibido: {mensaje}')
-
-# Programa principal, llamamos a la funcion
-saludar('Hola a todos')
