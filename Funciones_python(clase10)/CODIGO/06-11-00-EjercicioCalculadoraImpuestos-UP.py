51ed64eb1a2fd53d7341eb33772fba77294a31c4
Guillermo Nicolás Petcho <44329558+guillermopetcho@users.noreply.github.com>
2024-09-19 21:40:47 -0300

    Delete Funciones_python(clase10) directory

--- Funciones_python(clase10)/CODIGO/06-11-00-EjercicioCalculadoraImpuestos-UP.py
+++
@@ -1,5 +0,0 @@
-"""
-Ejercicio: Calculadora de Impuestos
-Crear una función para calcular el total de un pago incluyendo un impuesto aplicado.
-# Formula: pago_total = pago_sin_impuesto + pago_sin_impuesto * (impuesto/100)
-"""
--- Funciones_python(clase10)/Practicas/Modulo_conocimiento.py
+++
@@ -1,5 +0,0 @@
-## en el siguiente modulo vamos a trabajar con el modulo conocimiento
-
-#desde aqui gestionamos todas las preguntas y respuestas desde el main de conocimiento
-
-
