bfca12e97f290246c141460803cd0a5da8d770e2
Guillermo Nicolás Petcho <44329558+guillermopetcho@users.noreply.github.com>
2024-09-19 21:40:18 -0300

    Delete Sentencias_Ciclos(clase8) directory

--- Sentencias_Ciclos(clase8)/CODIGO/07-16-00-ValidacionCampoFormulario-UP.py
+++
@@ -1,9 +0,0 @@
-print('*** Validacion Campo de un Formulario ***')
-
-nombre_usuario = None
-
-while not nombre_usuario:
-    nombre_usuario = input('Ingresa tu nombre de usuario: ')
-
-print(f'Nombre de usuario válido: {nombre_usuario}')
-
--- Sentencias_Ciclos(clase8)/CODIGO/07-19-00-DibujarTriangulo-UP.py
+++
@@ -1,9 +0,0 @@
-print('*** Dibujar Triángulo Simétrico ***')
-
-numero_filas = int(input('Proporciona el número de filas: '))
-
-# Iterar sobre cada fila del triángulo
-for fila in range(1, numero_filas + 1):
-    espacios_blanco = ' ' * (numero_filas - fila)
-    asteriscos = '*' * (2 * fila - 1)
-    print(f'{espacios_blanco}{asteriscos}')
