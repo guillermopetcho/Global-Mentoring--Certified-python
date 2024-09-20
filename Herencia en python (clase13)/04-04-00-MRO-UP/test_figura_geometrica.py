47aaac9a96165ed0ef6ba3f80ecbba40e2f36237
Guillermo Nicolás Petcho <44329558+guillermopetcho@users.noreply.github.com>
2024-09-19 21:40:41 -0300

    Delete Herencia en python (clase13) directory

--- Herencia en python (clase13)/04-04-00-MRO-UP/test_figura_geometrica.py
+++
@@ -1,7 +0,0 @@
-from Cuadrado import Cuadrado
-
-cuadrado1 = Cuadrado(5, 'rojo')
-print(f'Cálculo área cuadrado: {cuadrado1.calcular_area()}')
-
-#MRO - Method Resolution Order
-print(Cuadrado.mro())
